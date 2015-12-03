1051b23a561060fa43e6eab44fb636ed97182989
Alfredo Gagliardi <gagliardi.alf@gmail.com>
2015-12-03 14:24:54 +0100

    Select random questions

--- services/question.js
+++ services/question.js
@@ -1,9 +1,10 @@
 module.exports = function (data) {
 	this.new = function () {
+		var index = Math.floor(Math.random() * data.people_it.length);
 		return {
-			id: data.people_it[0].id,
-			name: data.people_it[0].name,
-			desc: data.people_it[0].desc
+			id: data.people_it[index].id,
+			name: data.people_it[index].name,
+			desc: data.people_it[index].desc
 		};
 	};
 };
--- test/server/questionTest.js
+++ test/server/questionTest.js
@@ -19,10 +19,39 @@ describe('question', function () {
 	
 	describe('new', function () {
 		it('should return a question', function () {
+			var data = {
+				people_it: [
+					{id: 1, name: "name", desc: "desc", isAlive: true}
+				]
+			};
+			question = new Question(data);
 			var q = question.new();
 			(q.id).should.be.equal(1);
 			(q.name).should.be.equal("name");
 			(q.desc).should.be.equal("desc");
+		});
+		it('should select random questions', function (done) {
+			var p = [];
+			for (var j = 0; j < 1000; j++) {
+				p.push({id: j});
+			}
+			var data = {
+				people_it: p
+			};
+			question = new Question(data);
+			var ids = [];
+			for (var i = 0; i < 3; i++) {
+				setTimeout (function () {
+					var q = question.new();
+					if (ids.indexOf(q.id) == -1) {						
+						ids.push(q.id);
+					}
+				}, 10);	
+			};
+			setTimeout (function ()  {
+				(ids.length).should.be.equal(3);
+				done();
+			}, 100);
 		});
 	});
 });
