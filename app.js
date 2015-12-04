47af44768f49715f1c5fa2edb5e40782b99f885b
Alfredo Gagliardi <gagliardi.alf@gmail.com>
2015-12-05 00:32:49 +0100

    Changes for Openshift

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ var session = new Session();
 var question = new Question(data, session);
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 /*
@@ -52,8 +53,8 @@ app.get('/alive', function (req, res) {
 	});
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
 });
 
 module.exports = app;
