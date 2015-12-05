915f40827597484807cbe359ffb01d8fb75ec10a
Alfredo Gagliardi <gagliardi.alf@gmail.com>
2015-12-05 22:19:54 +0100

    Link services to Telegram

--- routes/telegram.js
+++ routes/telegram.js
@@ -18,17 +18,46 @@ router.post('/', function(req, res, next) {
     // Get the chat id and message id to reply to
     var chatId = telegramUpdate.message.chat.id;
     var reply_to_message_id = telegramUpdate.message.message_id;
+    var from_id = telegramUpdate.message.from.id;
     var replyMessage = '';
     if (telegramMessage.lastIndexOf('/start', 0) === 0) {
-    	replyMessage = 'Start command called';        
+    	session.start(from_id);
+	    question.new(from_id, function (err, data) {
+	    	if (err) {
+	    		replyMessage = 'Error while getting new question';
+	    	} else {
+
+	    	}
+		}); 
     } else if (telegramMessage.lastIndexOf('/end', 0) === 0) {
+    	session.stop(from_id);
     	replyMessage = 'End command called';
     } else if (telegramMessage.lastIndexOf('/new', 0) === 0) {
-    	replyMessage = 'New command called';
+    	question.new(from_id, function (err, data) {
+	    	if (err) {
+	    		replyMessage = 'Error while getting new question';
+	    	} else {
+	    		replyMessage = data.name + ',' + data.desc + '\nAlive or dead?';
+	    	}
+		});
     } else if (telegramMessage.lastIndexOf('/dead', 0) === 0) {
-    	replyMessage = 'Dead command called';
-   	} else if (telegramMessage.lastIndexOf('/start', 0) === 0) {
-   		replyMessage = 'Alive command called';
+    	question.answer(false, from_id, function (err, data) {
+			if (err) {
+	    		replyMessage = 'Error while answering';
+	    	} else {
+				replyMessage = data.feedback ? "Good!" : "No, that's wrong!";
+				replyMessage += "\nAnother one? Send \\new";
+	    	}
+		});
+   	} else if (telegramMessage.lastIndexOf('/alive', 0) === 0) {
+   		question.answer(true, from_id, function (err, data) {
+			if (err) {
+	    		replyMessage = 'Error while answering';
+	    	} else {
+				replyMessage = data.feedback ? "Good!" : "No, that's wrong!";
+				replyMessage += "\nAnother one? Send \\new";
+	    	}
+		});
     } else {
     	replyMessage = 'Unknown command';
     }
